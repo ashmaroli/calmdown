4eac3e20f8975bc77f1cb6cd71e4344695267747
Ashwin Maroli <ashmaroli@gmail.com>
2021-10-03 14:54:11 +0530

    Update workflow to diff two profile reports

--- .github/workflows/scripts/memprof.rb
+++ .github/workflows/scripts/memprof.rb
@@ -3,12 +3,18 @@
 require 'jekyll'
 require 'memory_profiler'
 
-MemoryProfiler.report(allow_files: ['lib/kramdown']) do
+reporter = MemoryProfiler.report(allow_files: ['lib/kramdown']) do
   Jekyll::PluginManager.require_from_bundler
   Jekyll::Commands::Build.process({
-    "source"             => File.expand_path(ARGV[0]),
-    "destination"        => File.expand_path("#{ARGV[0]}/_site"),
+    "source"             => File.expand_path("sandbox"),
+    "destination"        => File.expand_path("sandbox/_site"),
     "disable_disk_cache" => true,
   })
   puts ''
-end.pretty_print(scale_bytes: true, normalize_paths: true)
+end
+
+if ENV['KD_PATH']
+  reporter.pretty_print(to_file: "#{ARGV[0]}/memprof.txt", scale_bytes: true, normalize_paths: true)
+else
+  reporter.pretty_print(scale_bytes: true, normalize_paths: true)
+end
